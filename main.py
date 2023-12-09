d84e93c7c8eebf2f1f486eac9c556488f72f17b3
Mateusz <morveusz93@gmail.com>
2023-12-09 20:07:43 +0100

    token

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
 import yt_dlp as youtube_dl
+import os
 
 
-TOKEN = "MTE4MjczOTQ3MTAzMzE3MjA4MQ.GWelbj.Yln1aGKQ2dSvEUXx61qfgcU3H7oKak7Ia-VSR4"
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
