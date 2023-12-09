6bf1f3e428d5964dc18ef1d491b92a1868f68dec
Mateusz <morveusz93@gmail.com>
2023-12-09 20:45:16 +0100

    refacotr

---
+++ config.py
@@ -0,0 +1,10 @@
+ydl_opts = {
+    "format": "bestaudio",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ],
+}
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import yt_dlp as youtube_dl
 import os
+from config import ydl_opts
+from utils import get_playback_url
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -13,7 +16,7 @@ bot = commands.Bot(command_prefix="!d", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Zalogowano jako {bot.user.name}")
+    print(f"Logged in as {bot.user.name}")
 
 
 @bot.event
@@ -52,57 +55,35 @@ async def leave(ctx):
 async def play(ctx, url=None):
     if not url:
         return await ctx.send('You must provide a YouTube link: "!play <link>"')
-    voice_channel = ctx.author.voice.channel
 
-    if not voice_channel:
+    author_voice_channel = ctx.author.voice.channel
+    if not author_voice_channel:
         return await ctx.send("You must be in a voice channel to summon Dagon.")
 
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    bot_voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
-    if not voice_client or not voice_client.is_connected():
+    if not bot_voice_channel or not bot_voice_channel.is_connected():
         try:
-            voice_client = await voice_channel.connect()
+            bot_voice_channel = await author_voice_channel.connect()
         except Exception as e:
-            print(f"Wystąpił błąd: {e}")
-            return await ctx.send(
-                "Error occurred while summoning Dagon to the voice channel."
-            )
+            print(f"Error occurred: {e}")
+            return await ctx.send("Error occurred while summoning Dagon to the voice channel.")
 
-    elif voice_client.channel != voice_channel:
+    elif bot_voice_channel.channel != author_voice_channel:
         return await ctx.send("Dagon is already summoned to another voice channel.")
-
-    ydl_opts = {
-        "format": "bestaudio",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-    }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             info = ydl.extract_info(url, download=False)
         except youtube_dl.utils.DownloadError:
             return await ctx.send("Error occurred. Make sure the link is correct.")
-        formats = [
-            x for x in info["formats"] if x["resolution"].lower() == "audio only"
-        ]
-        defaults = [d for d in formats if "medium" in d["format"]]
-        if not defaults:
-            defaults = [d for d in formats if d.get("format_note") == "Default"]
-        if not defaults:
-            defaults = formats
-        try:
-            music_url = defaults[0].get("url")
-        except KeyError:
-            ctx.send(
-                "Error occurred while fetching music from the video, please provide another link."
-            )
-        voice_client.play(
-            discord.FFmpegPCMAudio(music_url),
+
+        playback_url = get_playback_url(info)
+        if not playback_url:
+            return await ctx.send("Error occurred while fetching music from the video, please provide another link.")
+
+        bot_voice_channel.play(
+            discord.FFmpegPCMAudio(playback_url),
             after=lambda e: print("Playback finished"),
         )
 
