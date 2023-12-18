38d686fe612fb06cc6e7de253c890093289d2dc7
Mateusz <morveusz93@gmail.com>
2023-12-18 09:46:43 +0100

    refacor

--- main.py
+++ main.py
@@ -1,152 +1,98 @@
-import discord
-from discord.ext import commands
-import yt_dlp as youtube_dl
-import os
-from config import ydl_opts
-from utils import get_playback_url
 import asyncio
+from config import ytdl_format_options, ffmpeg_options
+import discord
+import yt_dlp as youtube_dl
+
+from discord.ext import commands
+import os
+
+from dagon import Dagon
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!d", intents=intents)
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}")
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        audio_url = data['url']
+        return cls(discord.FFmpegPCMAudio(audio_url, **ffmpeg_options), data=data)
 
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    voice_client = discord.utils.get(bot.voice_clients, guild=member.guild)
-    if (
-        voice_client
-        and len(voice_client.channel.members) == 1
-        and bot.user in voice_client.channel.members
-    ):
-        await voice_client.disconnect()
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
+    @commands.command()
+    async def join(self, ctx):
+        await ctx.send("Dagon has been summoned!")
 
-@bot.command()
-async def join(ctx):
-    author_voice_channel = ctx.author.voice
-    if author_voice_channel and author_voice_channel.channel:
-        channel = author_voice_channel.channel
-        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    @commands.command()
+    async def play(self, ctx, *, url):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+
+            if ctx.voice_client.is_playing():
+                ctx.voice_client.stop()
+
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+            ctx.voice_client.source.volume = self.bot.vol / 100
+
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command()
+    async def vol(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+        if volume > 100 or volume < 0:
+            return await ctx.send("Volume must be in range (0-100).")
+        self.bot.vol = volume
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+
         if voice_client:
-            await voice_client.disconnect()
-        await channel.connect()
-        await ctx.send(f"Dagon has been summoned to the voice channel: {channel.name}")
-    else:
-        await ctx.send("You must be in a voice channel to summon Dagon.")
+            voice_client.stop()
+            await ctx.send("Playback stopped.")
 
+    @play.before_invoke
+    @join.before_invoke
+    async def ensure_voice(self, ctx):
+        current_bot_channel = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        if current_bot_channel and current_bot_channel != ctx.author.voice:
+            await current_bot_channel.disconnect()
 
-@bot.command()
-async def leave(ctx):
-    voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice_channel and voice_channel.is_connected():
-        await voice_channel.disconnect()
-
-
-@bot.command()
-async def play(ctx, url=None):
-    if not url:
-        return await ctx.send('You must provide a YouTube link: "!play <link>"')
-
-    if not ctx.author.voice:
-        return await ctx.send("You must be in a voice channel to summon Dagon.")
-    author_voice_channel = ctx.author.voice.channel
-
-    bot_voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if not bot_voice_channel or not bot_voice_channel.is_connected():
-        try:
-            bot_voice_channel = await author_voice_channel.connect()
-            await ctx.send("Summoning Dagon to play music!")
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            return await ctx.send(
-                "Error occurred while summoning Dagon to the voice channel."
-            )
-
-    elif bot_voice_channel.channel != author_voice_channel:
-        return await ctx.send("Dagon is already summoned to another voice channel.")
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info = ydl.extract_info(url, download=False)
-        except youtube_dl.utils.DownloadError:
-            return await ctx.send("Error occurred. Make sure the link is correct.")
-
-        if "entries" in info:
-            # playlist
-            playlist_url = url
-            for entry in info["entries"]:
-                if url := entry.get("url"):
-                    try:
-                        info = ydl.extract_info(url, download=False)
-                    except youtube_dl.utils.DownloadError:
-                        return await ctx.send(
-                            "Error occurred. Make sure the link is correct."
-                        )
-                    playback_url = get_playback_url(info)
-                    if playback_url:
-                        bot_voice_channel.play(
-                            discord.FFmpegPCMAudio(playback_url),
-                            after=lambda e: print("Playback finished"),
-                        )
-                        while bot_voice_channel.is_playing():
-                            await asyncio.sleep(1)
-                        await ctx.send("Dagon plays music!")
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
         else:
-            # one video
-            playback_url = get_playback_url(info)
-            if not playback_url:
-                return await ctx.send(
-                    "Error occurred while fetching music from the video, please provide another link."
-                )
-
-            bot_voice_channel.play(
-                discord.FFmpegPCMAudio(playback_url),
-                after=lambda e: print("Playback finished"),
-            )
-            await ctx.send("Dagon plays music!")
+            await ctx.send("You are not connected to a voice channel.")
+            raise commands.CommandError("Author not connected to a voice channel.")
 
 
-@bot.command()
-async def stop(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if voice_client:
-        voice_client.stop()
-        await ctx.send("Playback stopped.")
-    else:
-        await ctx.send("Dagon is not currently playing any music.")
+async def main():
+    bot = Dagon()
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start(TOKEN)
 
 
-@bot.command()
-async def pause(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if voice_client and voice_client.is_playing():
-        voice_client.pause()
-        await ctx.send("Playback paused. Use !resume to continue or !play to play next track.")
-    else:
-        await ctx.send("Dagon is not currently playing any music.")
-
-
-@bot.command()
-async def resume(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-    if voice_client and voice_client.is_paused():
-        voice_client.resume()
-        await ctx.send("Playback resumed.")
-    else:
-        await ctx.send("Dagon is not currently paused.")
-
-
-bot.run(TOKEN)
+asyncio.run(main())
